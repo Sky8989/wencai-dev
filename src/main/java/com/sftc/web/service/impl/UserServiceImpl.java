81db23e9170002f9b90805782c070ba255bb50cb
SecondEnd <375194519@qq.com>
2017-05-25 10:43:18 +0800

    修复登录接口

--- src/main/java/com/sftc/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/sftc/web/service/impl/UserServiceImpl.java
@@ -1,12 +1,8 @@
 package com.sftc.web.service.impl;
 
 import com.sftc.tools.api.*;
-import com.sftc.tools.md5.MD5Util;
-
-import com.sftc.web.model.User;
-
 import com.sftc.web.model.Token;
-
+import com.sftc.web.model.User;
 import com.sftc.web.model.wechat.WechatUser;
 import com.sftc.web.service.UserService;
 import org.springframework.stereotype.Service;
@@ -27,39 +23,22 @@ public class UserServiceImpl extends AbstractBasicService implements UserService
 
     public APIResponse login(APIRequest request) throws Exception {
         APIStatus status = APIStatus.SUCCESS;
-        String user_phone = (String) request.getParameter("user_phone");
-        String user_password = MD5Util.MD5((String) request.getParameter("user_password"));
         String js_code = (String) request.getParameter("js_code");
         User user = null;
-        WechatUser wechatUser = null;
-        // 判断通过微信进行登录
-        if (js_code != null) {
-            AUTHORIZATION_URL = AUTHORIZATION_URL.replace("JSCODE", js_code);
-            wechatUser = APIResolve.getJson(AUTHORIZATION_URL);
-            System.out.println(wechatUser.getOpenid());
-            if (wechatUser.getOpenid() != null) {
-                user = userMapper.selectUserByOpenid(wechatUser.getOpenid());
-                if (user == null) {
-                    userMapper.insertOpenid(wechatUser.getOpenid());
-                }
-            } else {
-                status = APIStatus.WECHAT_ERR;
-                status.setState(wechatUser.getErrcode().toString());
-                status.setMessage(wechatUser.getErrmsg());
-            }
-
-            // 判断通过普通用户输入手机号密码登录
-        } else if (user_phone != null && user_password != null) {
-            user = userMapper.selectUserByPhone(user_phone);
+        AUTHORIZATION_URL = AUTHORIZATION_URL.replace("JSCODE", js_code);
+        WechatUser wechatUser = APIResolve.getJson(AUTHORIZATION_URL);
+        System.out.println(wechatUser.getOpenid());
+        if (wechatUser.getOpenid() != null) {
+            user = userMapper.selectUserByOpenid(wechatUser.getOpenid());
             if (user == null) {
-                status = APIStatus.USER_NOT_EXIST;
-            } else {
-                if (!user_password.equals(user.getUser_password())) {
-                    status = APIStatus.USER_FAIL;
-                }
+                userMapper.insertOpenid(wechatUser.getOpenid());
             }
+        } else {
+            status = APIStatus.WECHAT_ERR;
+            status.setState(wechatUser.getErrcode().toString());
+            status.setMessage(wechatUser.getErrmsg());
         }
-        return APIUtil.getResponse(status, user);
+            return APIUtil.getResponse(status, user);
     }
 
     public Token getToken(int id) {
