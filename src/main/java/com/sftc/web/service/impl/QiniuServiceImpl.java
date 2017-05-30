faf0efafd7f7e89f7a4c8432284a056e194a914a
SecondEnd <375194519@qq.com>
2017-05-30 16:55:07 +0800

    修改七牛接口

--- src/main/java/com/sftc/web/service/impl/QiniuServiceImpl.java
+++ src/main/java/com/sftc/web/service/impl/QiniuServiceImpl.java
@@ -27,9 +27,9 @@ public class QiniuServiceImpl implements QiniuService {
         Map<String, String> map = new HashMap<String, String>();
         Configuration configuration = new Configuration(Zone.zone2());
         UploadManager manager = new UploadManager(configuration);
-        String accessKey = "BGeLMmH8aau4OoPh3XNtJBRBl8-tC2qHLPJeAAxR";
-        String secretKey = "pHqfRtdemzuKsHNfMd1zGZbZX2cx17OBBMbV0HLu";
-        String bucket = "keming";
+        String accessKey = "Jyi6Ntprm38nI6n1heGjwXyQmzie8ZjY7l9Cq_Je";
+        String secretKey = "eBqe82USZuB6gnqvjTsyg_qki6C6HwnkFIvNIH-y";
+        String bucket = "sftc";
         Auth auth = Auth.create(accessKey, secretKey);
         String upToken = auth.uploadToken(bucket);
         map.put("uptoken", upToken);
