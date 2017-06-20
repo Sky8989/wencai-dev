0927b6eb23c10992b5263532355c3ceed2775442
starmoon1994@163.com <starmoon1994@163.com>
2017-06-20 17:05:40 +0800

    1、修改service寄件人填写订单模块 2、OrderParam新增两个字段

--- src/main/java/com/sftc/web/mapper/OrderMapper.java
+++ src/main/java/com/sftc/web/mapper/OrderMapper.java
@@ -3,6 +3,7 @@ package com.sftc.web.mapper;
 import com.sftc.web.model.Order;
 import com.sftc.web.model.OrderExpress;
 import com.sftc.web.model.apiCallback.OrderCallback;
+import org.aspectj.weaver.ast.Or;
 
 import java.util.List;
 
@@ -14,7 +15,6 @@ public interface OrderMapper {
 
     void addOrder(Order order);
 
-
     void updateOrder(Order order);
     //通过订单Id修改订单信息，只修改订单类型
     void updateOrderTypeById(Order Order);
@@ -23,7 +23,6 @@ public interface OrderMapper {
     Order orderAndOrderExpressAndGiftDetile(int id);
 
     List<Order> myOrderList(Order order);
-
 
     int findPackageCount(String order_number);
 
