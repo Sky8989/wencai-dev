4c1cbdfd9bd86d9de4a03f5479485d24da0d6478
bingozb <454113692@qq.com>
2017-06-25 17:57:14 +0800

    好友来往记录接口

--- src/main/java/com/sftc/web/mapper/UserContactMapper.java
+++ src/main/java/com/sftc/web/mapper/UserContactMapper.java
@@ -7,24 +7,13 @@ import com.sftc.web.model.reqeustParam.UserContactParam;
 
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- *
- * @author _KeMing
- * @version 1.0
- * @Package com.sftc.web.mapper
- * @Description:
- * @date 2017/4/7
- * @Time 上午8:44
- */
+
 public interface UserContactMapper {
 
     // 好友列表
     List<UserContact> friendList(Paging paging);
 
     int selectFriendCount(int user_id);
-
-    String selectUserIcon(int id);
 
     // 添加好友
     void addFriend(UserContactParam userContactParam);
--- src/main/java/com/sftc/web/model/reqeustParam/UserContactParam.java
+++ src/main/java/com/sftc/web/model/reqeustParam/UserContactParam.java
@@ -1,90 +1,18 @@
 package com.sftc.web.model.reqeustParam;
 
-import com.sftc.web.model.Object;
-
-/**
- * Created by IntelliJ IDEA.
- *
- * @author _KeMing
- * @version 1.0
- * @Package com.sftc.web.model.reqeustParam
- * @Description: 用户以及好友圈的包装类
- * @date 2017/5/22
- * @Time 上午11:00
- */
-public class UserContactParam extends Object {
-
-    private int id;
-    // 好友姓名
-    private String name;
-    // 好友头像
-    private String icon;
-    // 好友电话
-    private String phone;
-    // 微信号
-    private String wechat;
-    // 好友备注
-    private String remark;
-    // 创建时间
-    private String create_time;
+public class UserContactParam {
+    private String access_token;
     private int user_id;
     private int friend_id;
-    private String token;
+    private int pageNum;
+    private int pageSize;
 
-    public int getId() {
-        return id;
+    public String getAccess_token() {
+        return access_token;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getWechat() {
-        return wechat;
-    }
-
-    public void setWechat(String wechat) {
-        this.wechat = wechat;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public String getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(String create_time) {
-        this.create_time = create_time;
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
     }
 
     public int getUser_id() {
@@ -103,11 +31,19 @@ public class UserContactParam extends Object {
         this.friend_id = friend_id;
     }
 
-    public String getToken() {
-        return token;
+    public int getPageNum() {
+        return pageNum;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
     }
 }
