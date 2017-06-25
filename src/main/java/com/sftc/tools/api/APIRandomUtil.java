785a3978745220d35364aac3f5d1739b55011371
bingozb <454113692@qq.com>
2017-06-26 02:15:36 +0800

    订单号长度改为12位

--- src/main/java/com/sftc/tools/api/APIRandomUtil.java
+++ src/main/java/com/sftc/tools/api/APIRandomUtil.java
@@ -6,18 +6,24 @@ import java.util.Random;
 
 public class APIRandomUtil {
 
-    private static final int ORDER_LENGTH = 9;
+    private static final int ORDER_LENGTH = 12;
 
     private static String getRandomString(int length) {
         String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < length; ++i) {
-            int number = random.nextInt(62);
+            int number = random.nextInt(str.length());
             sb.append(str.charAt(number));
         }
+        String result = MD5Util.MD5(sb.toString());
 
-        return MD5Util.MD5(sb.toString());
+        if (result != null && result.length() > ORDER_LENGTH) {
+            result = result.substring(0, ORDER_LENGTH);
+        }
+
+        return result;
     }
 
     public static String getRandomString() {
