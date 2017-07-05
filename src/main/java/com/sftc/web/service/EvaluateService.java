21ce6f4eef25578cda1d5e26c6b5a6161832ca55
starmoon1994@163.com <starmoon1994@163.com>
2017-07-05 09:53:17 +0800

    新建 CMS快递评价列表 接口

--- src/main/java/com/sftc/web/service/EvaluateService.java
+++ src/main/java/com/sftc/web/service/EvaluateService.java
@@ -2,10 +2,14 @@ package com.sftc.web.service;
 
 import com.sftc.tools.api.APIResponse;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Administrator on 2017/6/20.
  */
 
 public interface EvaluateService {
     public APIResponse getEvaluate(String uuid);
+
+    public APIResponse selectEvaluateListByPage(HttpServletRequest httpServletRequest);
 }
