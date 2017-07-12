e3520b0c7fae27dd48d6094129f90cfafd359f67
starmoon1994@163.com <starmoon1994@163.com>
2017-07-12 15:53:28 +0800

    修改接口 我的订单 内置token

--- src/main/java/com/sftc/tools/sf/SFTokenHelper.java
+++ src/main/java/com/sftc/tools/sf/SFTokenHelper.java
@@ -9,6 +9,7 @@ import static com.sftc.tools.constant.SFConstant.SF_TOKEN_URL;
 public class SFTokenHelper {
     /**
      * 顺丰共用access_token
+     * 使用本token的接口：来往记录 好友圈 我的订单
      */
     public final static String COMMON_ACCESSTOKEN = "padHjjRvusAC9z7ehxpG";
 
