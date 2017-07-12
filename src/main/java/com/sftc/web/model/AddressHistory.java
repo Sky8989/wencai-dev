f207c0fc8e3ce772a10d0218032c87e9f5cb3246
starmoon1994@163.com <starmoon1994@163.com>
2017-07-12 11:47:05 +0800

    历史地址添加用户的微信名

--- src/main/java/com/sftc/web/model/AddressHistory.java
+++ src/main/java/com/sftc/web/model/AddressHistory.java
@@ -13,63 +13,40 @@ public class AddressHistory extends Object {
     private int is_mystery;
     // 创建时间
     private String create_time;
-
+    // 好友微信名
+    private String ship_wechatname;
     // 地址
     private Address address;
 
-    public int getId() {
-        return id;
-    }
+    public int getId() {return id;}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) {this.id = id;}
 
-    public int getUser_id() {
-        return user_id;
-    }
+    public int getUser_id() {return user_id;}
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
+    public void setUser_id(int user_id) {this.user_id = user_id;}
 
-    public int getAddress_id() {
-        return address_id;
-    }
+    public int getAddress_id() {return address_id;}
 
-    public void setAddress_id(int address_id) {
-        this.address_id = address_id;
-    }
+    public void setAddress_id(int address_id) {this.address_id = address_id;}
 
-    public int getIs_delete() {
-        return is_delete;
-    }
+    public int getIs_delete() {return is_delete;}
 
-    public void setIs_delete(int is_delete) {
-        this.is_delete = is_delete;
-    }
+    public void setIs_delete(int is_delete) {this.is_delete = is_delete;}
 
-    public int getIs_mystery() {
-        return is_mystery;
-    }
+    public int getIs_mystery() {return is_mystery;}
 
-    public void setIs_mystery(int is_mystery) {
-        this.is_mystery = is_mystery;
-    }
+    public void setIs_mystery(int is_mystery) {this.is_mystery = is_mystery;}
 
-    public String getCreate_time() {
-        return create_time;
-    }
+    public String getCreate_time() {return create_time;}
 
-    public void setCreate_time(String create_time) {
-        this.create_time = create_time;
-    }
+    public void setCreate_time(String create_time) {this.create_time = create_time;}
 
-    public Address getAddress() {
-        return address;
-    }
+    public Address getAddress() {return address;}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+    public void setAddress(Address address) {this.address = address;}
+
+    public String getShip_wechatname() {return ship_wechatname;}
+
+    public void setShip_wechatname(String ship_wechatname) {this.ship_wechatname = ship_wechatname;}
 }
