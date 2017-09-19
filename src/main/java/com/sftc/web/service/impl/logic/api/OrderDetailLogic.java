391a1dfee4e1e1e0adddc21b87909c654b971d58
bingozb <454113692@qq.com>
2017-09-19 15:05:57 +0800

    只有到付的同城已支付但未填验证码才改为待揽件

--- src/main/java/com/sftc/web/service/impl/logic/api/OrderDetailLogic.java
+++ src/main/java/com/sftc/web/service/impl/logic/api/OrderDetailLogic.java
@@ -152,7 +152,7 @@ public class OrderDetailLogic {
             }
 
             boolean payed = respObject.getJSONObject("request").getBoolean("payed");
-            if (payed && order_status.equals("PAYING")) {
+            if (payed && order_status.equals("PAYING") && order.getPay_method().equals("FREIGHT_COLLECT")) {
                 respObject.getJSONObject("request").put("status", "WAIT_HAND_OVER");
                 orderExpressMapper.updateOrderExpressStatusByUUID(uuid, "WAIT_HAND_OVER");
                 order = orderMapper.selectOrderDetailByUuid(uuid);
