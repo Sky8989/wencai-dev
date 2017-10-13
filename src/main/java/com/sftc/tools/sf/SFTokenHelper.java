5a1e47ae6270cb6652b68dc8e2dfc8119766e538
unknown <>
2017-10-14 07:01:37 +0800

    公共token

--- src/main/java/com/sftc/tools/sf/SFTokenHelper.java
+++ src/main/java/com/sftc/tools/sf/SFTokenHelper.java
@@ -11,8 +11,8 @@ public class SFTokenHelper {
      * 顺丰共用access_token
      * 使用本token的接口：来往记录 好友圈 我的订单
      */
-    public static String COMMON_ACCESSTOKEN = "0UjuP51pdFBv5KKvPitx";
-    public static String COMMON_UUID = "2c9a85895d82ebe7015d8d4c6cc11df6";
+    public static String COMMON_ACCESSTOKEN = "EyMivbd44I124lcddrBG";
+    public static String COMMON_UUID = "2c9a85895d78ae17015d7911fa810029";
 
     public static String getToken() {
 
