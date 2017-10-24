ee27f8247719420b7244a8eded000fdd114d0d58
unknown <>
2017-10-24 11:56:30 +0800

    修改更新用户信息的东西

--- src/main/java/com/sftc/web/model/Converter/TimeStartVoid.java
+++ src/main/java/com/sftc/web/model/Converter/TimeStartVoid.java
@@ -5,9 +5,13 @@ import com.sftc.web.dao.mybatis.OrderMapper;
 import com.sftc.web.model.OrderNationTime;
 import com.sftc.web.service.impl.logic.app.OrderCommitLogic;
 import com.sftc.web.service.impl.logic.app.OrderTimerLogic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.web.context.ServletContextAware;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.util.Timer;
@@ -19,35 +23,30 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by xf on 2017/10/23.
  */
-public class TimeStartVoid implements InitializingBean,ServletContextListener {
+public class TimeStartVoid implements InitializingBean,ServletContextAware {
     @Resource
     private OrderTimerLogic orderTimerLogic;
     @Resource
     private OrderMapper orderMapper;
     @Resource
     private OrderCommitLogic orderCommitLogic;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private Timer timer = null;
     public void afterPropertiesSet() throws Exception {
-        System.out.println("aaa");
+        logger.info("定时器开启");
         APIRequest request = new APIRequest();
         OrderNationTime orderNationTime = OrderTimeFactory.setOrderNationTimeOn();
         request.setRequestParam(orderNationTime);
         orderTimerLogic.setupReserveNationOrderCommitTimer(request);
-    }
-
-
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
         timer = new Timer(true);
-        sce.getServletContext().log("定时器已启动");
         long period = 1800000;
         long delay = 0;
         timer.schedule(new MyTimeTask(orderMapper,orderCommitLogic),delay,period);
-        sce.getServletContext().log("定时器已启动");
+        logger.info("定时器开启");
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce) {
+    public void setServletContext(ServletContext servletContext) {
 
     }
 }
