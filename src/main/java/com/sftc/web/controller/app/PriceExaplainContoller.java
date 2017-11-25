5b7840dd36275c0f005f3ab7a47bec57d4934623
wencai <wencai@dankal.cn>
2017-11-25 17:11:28 +0800

    根据城市获取价格说明 接口  去掉多余参数

--- src/main/java/com/sftc/web/controller/app/PriceExaplainContoller.java
+++ src/main/java/com/sftc/web/controller/app/PriceExaplainContoller.java
@@ -16,7 +16,6 @@ import com.sftc.web.model.vo.swaggerRequest.DeletePriceExplain;
 import com.sftc.web.service.PriceExaplainService;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 @Api(description = "CMS价格说明")
@@ -26,7 +25,6 @@ public class PriceExaplainContoller {
     @Autowired
     private PriceExaplainService priceExaplainService;
     @ApiOperation(value = "CMS根据城市获取价格说明", httpMethod = "POST")
-    @ApiImplicitParam(name = "city", value = "城市", required = true, paramType = "query", defaultValue = "北京")
     @PostMapping(value = "get")
     public APIResponse getPriceExplainByCity(@RequestBody PriceExaplainVO priceExaplainVO) {
         APIRequest apiRequest = new APIRequest();
